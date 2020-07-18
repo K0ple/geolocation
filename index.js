499e20fb7a3e6205817084d1f1316fc7466d5b79
Satyaki Suman <satyaki.suman@gmail.com>
2020-07-18 22:06:09 +0530

    Added functionality to log clients location

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var axios = require('axios');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.get('/location/:id', (req, res) => {
+    var config = {
+        method: 'get',
+        url: 'http://ipwhois.app/json/',
+        headers: { }
+    };
+    axios(config)
+    .then(function (response) {
+        //console.log(JSON.stringify(response.data));
+        console.log(response.data.latitude);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    var id = req.params.id;
+    console.log(id);
+    res.send('Thanks for reporting to us this location!');
+})
 
 app.listen(port, () => console.log(`Example app listening on port port!`))
